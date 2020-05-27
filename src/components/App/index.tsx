47efae810296df7a9270a29d1ca2694fb5073b85
Adrian <adriantoddross@gmail.com>
2020-05-27 09:45:23 -0400

    Stub number display

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import "./App.scss";
+import NumberDisplay from "../NumberDisplay";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <div className="Header">Header</div>
+      <div className="Header">
+        <NumberDisplay value={0} />
+        <NumberDisplay value={23} />
+      </div>
       <div className="Body">Body</div>
     </div>
   );
