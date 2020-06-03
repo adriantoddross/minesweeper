f96cf9eb31cb89c5d59178bf4687d1e80b82d040
Adrian <adriantoddross@gmail.com>
2020-06-03 10:01:41 -0400

    Randomly place bombs inside of cells!

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,39 @@
-import { MAX_COLUMNS, MAX_ROWS } from "../constants";
+import { MAX_COLUMNS, MAX_ROWS, NUMBER_OF_BOMBS } from "../constants";
 import { CellValue, CellState, Cell } from "../types";
 
 export const generateCells = () => {
-  const cells: Cell[][] = [];
+  let cells: Cell[][] = [];
 
+  // Generate all cells
   for (let row = 0; row < MAX_ROWS; row++) {
     cells.push([]);
     for (let col = 0; col < MAX_COLUMNS; col++) {
       cells[row].push({
         value: CellValue.none,
-        state: CellState.hidden
-      })
+        state: CellState.hidden,
+      });
+    }
+  }
+  // Randomly place 10 bombs.
+  let bombsPlaced = 0;
+  while (bombsPlaced < NUMBER_OF_BOMBS) {
+    const randomRow = Math.floor(Math.random() * MAX_ROWS);
+    const randomColumn = Math.floor(Math.random() * MAX_COLUMNS);
+
+    const currentCell = cells[randomRow][randomColumn];
+    if (currentCell.value !== CellValue.bomb) {
+      cells = cells.map((row, rowIndex) =>
+        row.map((cell, columnIndex) => {
+          if (randomRow === rowIndex && randomColumn === columnIndex) {
+            return {
+              ...cell,
+              value: CellValue.bomb,
+            };
+          }
+          return cell;
+        })
+      );
+      bombsPlaced++;
     }
   }
   return cells;
