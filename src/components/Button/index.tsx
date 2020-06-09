37ac84ffb0af0473d6d2b3fbee63148ec5c6d4af
Adrian <adriantoddross@gmail.com>
2020-06-09 09:47:40 -0400

    Render a few emoji based on CellValue; 27:27

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,37 @@
 import React from "react";
 
 import "./Button.scss";
+import { CellState, CellValue } from "../../types";
 
-const Button: React.FC = () => {
-  return <div className="Button"/>;
+interface ButtonProps {
+  row: number;
+  column: number;
+  state: CellState;
+  value: CellValue;
+}
+
+const Button: React.FC<ButtonProps> = ({ row, column, state, value }) => {
+  const renderContent = (): React.ReactNode => {
+    if (state === CellState.visible) {
+      if (value === CellValue.bomb) {
+        return (
+          <span role="img" className="Bomb" aria-label="bomb emoji">
+            💣
+          </span>
+        );
+      }
+    } else if (state === CellState.flagged) {
+      return <span role="img" className="Bomb" aria-label="bomb emoji">
+        🚩
+      </span>;
+    }
+    return null;
+  };
+  return (
+    <div className={`Button ${state === CellState.visible ? "visible" : ""} `}>
+      {renderContent()}
+    </div>
+  );
 };
 
 export default Button;
